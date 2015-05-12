b14a7564bf506c792edb78c2f7bf0767c06e63e5
Andres De Leon <agldeleon@gmail.com>
2015-05-12 17:42:14 -0500

    Update routes for the products

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, path: '/api' do
     api_version(:module => "V1", :header => {:name => "Accept", :value => "application/vnd.list.com+json; version=1"}, :defaults => {:format => :json}, :default => true) do
-      #resources :tasks, except: [:new, :edit]
+      resources :products, except: [:new, :edit]
 
       #Sessions handling
       #post "sing_in" => "sessions#create"
