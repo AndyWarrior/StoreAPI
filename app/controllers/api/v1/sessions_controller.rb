6addfc4684ef0cb5f47510c0f38505ad99deced7
Andres De Leon <agldeleon@gmail.com>
2015-05-12 19:50:18 -0500

    Add authentication to users

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::SessionsController < Api::V1::BaseController
+	include Authenticable
 	before_action :authenticate, only: [:destroy]
 
 	def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   namespace :api, path: '/api' do
     api_version(:module => "V1", :header => {:name => "Accept", :value => "application/vnd.list.com+json; version=1"}, :defaults => {:format => :json}, :default => true) do
       resources :products, except: [:new, :edit]
 
       #Sessions handling
-      #post "sing_in" => "sessions#create"
-      #delete "sign_out" => "sessions#destroy"
+      post "sign_in" => "sessions#create"
+      delete "sign_out" => "sessions#destroy"
     end
   end
 end
